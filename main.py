7e778604ee3ae5a51c277da65f616fe934b633bd
Stat-X <evgenuradeutsch88@gmail.com>
2025-12-29 16:42:11 +0100

    First commit. Creating of Bot and checking polling

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, Router, types
 
+
+load_dotenv()
+
+
+bot = Bot(token=os.getenv("TOKEN"))
+dp = Dispatcher()
+
+@dp.message()
+async def echo_handler(message: types.Message):
+    await message.answer(message.text)
+    
+router = Router()
+
+@router.message("HI")
+async def echo_handler(message: types.Message):
+    await message.answer("Hello!")
+
+async def main():
+    # dp.include_router(router)
+    await router.start_polling(bot)
+    
+    
+if __name__ == "__main__":
+    asyncio.run(main())
