bc537adca2654d1f33c15fa62b42d1013292c091
Stat-X <evgenuradeutsch88@gmail.com>
2026-01-01 23:13:13 +0100

    small corrections

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,12 @@
 from handlers.welcome_router import router as welcome_router
-from handlers.anti_link      import router as link_router
+from handlers.anti_link      import router as antispam_router
 from handlers.goodby_router  import router as farewell_router
 
 
 
 __all__ = [
     "welcome_router",
-    "link_router",
+    "antispam_router",
     "farewell_router",
     "advertisment_router"
     ]
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import os
 import asyncio
-from dotenv   import load_dotenv
 from aiogram  import Bot, Dispatcher
-from handlers import welcome_router, link_router, farewell_router
+from handlers import welcome_router, antispam_router, farewell_router
 from database import create_db, create_db_posts
+from env_collection import TOKEN          
 
 
-load_dotenv()
-
-bot = Bot(token=os.getenv("TOKEN"))
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 async def main():
@@ -16,7 +13,7 @@ async def main():
     await create_db_posts()
     dp.include_router(welcome_router)
     dp.include_router(farewell_router)
-    dp.include_router(link_router)
+    dp.include_router(antispam_router)
     await dp.start_polling(bot)
     
 if __name__ == "__main__":
